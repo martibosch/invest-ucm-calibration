118ccef42ff4ae7f3fe5d1957a75f18aefd56251
Martí Bosch <marti.bosch@epfl.ch>
2020-09-11 11:36:57 +0200

    Bump version: 0.4.0 → 0.4.1

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -69,7 +69,7 @@ author = "Martí Bosch"
 # the built documents.
 #
 # The short X.Y version.
-__version__ = '0.4.0'
+__version__ = '0.4.1'
 version = __version__
 # The full version, including alpha/beta/rc tags.
 release = __version__
