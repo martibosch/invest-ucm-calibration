0be581a811eb1e597c0ed0bf17966dc4fd096967
Martí Bosch <marti.bosch@protonmail.com>
2022-12-14 16:15:12 +0100

    feat: ppw setup with invest>=3.10.1 (conda-forge, gdal 3...)

---
+++ docs/usage.md
@@ -0,0 +1,7 @@
+# Usage
+
+To use InVEST UCM calibration in a project
+
+```
+    import invest_ucm_calibration
+```
---
+++ tests/_generate_test_data.py
@@ -0,0 +1,156 @@
+import os
+import string
+from os import path
+
+import numpy as np
+import osr
+import pandas as pd
+import rasterio as rio
+from numpy import random as rn
+from rasterio import transform
+
+
+def dump_rasters(raster_filepaths, low, high, nodata, data_mask, dtype, meta):
+    for raster_filepath in raster_filepaths:
+        with rio.open(raster_filepath, "w", dtype=dtype, nodata=nodata, **meta) as dst:
+            dst_arr = (
+                rn.uniform(low, high, meta["height"] * meta["width"])
+                .reshape(meta["height"], meta["width"])
+                .astype(dtype)
+            )
+            dst.write(np.where(data_mask, dst_arr, nodata), 1)
+
+
+tests_data_dir = "tests/data"
+
+if not path.exists(tests_data_dir):
+    os.mkdir(tests_data_dir)
+
+# basic config
+width, height = 5, 5
+num_dates = 2
+shape = (height, width)
+num_pixels = width * height
+west, south, east, north = 0, 0, 10, 10
+srs = osr.SpatialReference()
+srs.ImportFromEPSG(4326)
+meta = dict(
+    driver="GTiff",
+    width=width,
+    height=height,
+    count=1,
+    transform=transform.from_bounds(west, south, east, north, width, height),
+    crs=srs.ExportToProj4(),
+)
+
+data_mask = np.full(shape, True)
+data_mask[0, 0] = False
+
+lulc_raster_filepath = path.join(tests_data_dir, "lulc.tif")
+num_lulc_classes = 4
+lulc_nodata = 255
+lulc_dtype = np.uint8
+
+biophysical_table_filepath = path.join(tests_data_dir, "biophysical-table.csv")
+
+# aoi_vector_filepath = path.join(tests_data_dir, 'aoi-vector.shp')
+
+ref_et_raster_filepaths = [
+    path.join(tests_data_dir, f"ref_et{i}.tif") for i in range(num_dates)
+]
+ref_et_low = 1
+ref_et_high = 3
+ref_et_nodata = -1
+ref_et_dtype = np.float32
+
+t_raster_filepaths = [path.join(tests_data_dir, f"T{i}.tif") for i in range(num_dates)]
+unaligned_t_raster_filepaths = [
+    path.join(tests_data_dir, f"_T{i}.tif") for i in range(num_dates)
+]
+t_low = 20
+t_high = 30
+t_nodata = -300
+t_dtype = np.float32
+
+num_stations = 2
+station_labels = [letter for letter in string.ascii_lowercase[:num_stations]]
+
+# dump a randomly-generated lulc raster
+with rio.open(
+    lulc_raster_filepath, "w", dtype=lulc_dtype, nodata=lulc_nodata, **meta
+) as dst:
+    dst_arr = np.digitize(
+        rn.random(num_pixels).reshape(shape), np.linspace(0, 1, 4)
+    ).astype(lulc_dtype)
+    dst.write(np.where(data_mask, dst_arr, lulc_nodata), 1)
+
+# dump a randomly-generated biophysical table
+biophysical_df = pd.DataFrame()
+biophysical_df["lucode"] = np.arange(1, num_lulc_classes + 1)
+for column in "kc", "albedo", "shade":
+    biophysical_df[column] = rn.random(num_lulc_classes)
+biophysical_df["green_area"] = rn.choice([0, 1], num_lulc_classes)
+biophysical_df.to_csv(biophysical_table_filepath)
+
+# # dump an aoi vector shapefile with a single geometry (bounding box)
+# # based on Mike T answer in stackoverflow https://bit.ly/3dp7Ihb
+# # Define a polygon feature geometry with one attribute
+# schema = {
+#     'geometry': 'Polygon',
+#     'properties': {
+#         'id': 'int'
+#     },
+# }
+# with fiona.open(aoi_vector_filepath, mode='w', driver='ESRI Shapefile',
+#                 schema=schema, crs=meta['crs']) as dst:
+#     ## If there are multiple geometries, put the "for" loop here
+#     dst.write({
+#         'geometry':
+#         geometry.mapping(geometry.box(west, south, east, north)),
+#         'properties': {
+#             'id': 123
+#         },
+#     })
+
+# dump a randomly-generated ref et raster
+dump_rasters(
+    ref_et_raster_filepaths,
+    ref_et_low,
+    ref_et_high,
+    ref_et_nodata,
+    data_mask,
+    ref_et_dtype,
+    meta,
+)
+
+# dump a randomly-generated t raster
+dump_rasters(t_raster_filepaths, t_low, t_high, t_nodata, data_mask, t_dtype, meta)
+
+# dump a randomly-generated unaligned t raster
+unaligned_meta = meta.copy()
+unaligned_shape = height - 1, width - 1
+unaligned_meta.update(width=width - 1, height=height - 1)
+dump_rasters(
+    unaligned_t_raster_filepaths,
+    t_low,
+    t_high,
+    t_nodata,
+    data_mask[1:, 1:],
+    t_dtype,
+    unaligned_meta,
+)
+
+# dump a randomly-generated station location data frame
+station_location_df = pd.DataFrame(index=station_labels)
+station_location_df["x"] = rn.uniform(west, east, num_stations)
+station_location_df["y"] = rn.uniform(south, north, num_stations)
+station_location_df.to_csv(path.join(tests_data_dir, "station-locations.csv"))
+
+# dump the randomly-generated station measurements data frames
+station_t_df = pd.DataFrame(columns=station_labels)
+station_t_df.loc[0] = rn.uniform(t_low, t_high, num_stations)
+# one date only
+station_t_df.to_csv(path.join(tests_data_dir, "station-t-one-day.csv"))
+# two dates
+station_t_df.loc[1] = rn.uniform(t_low, t_high, num_stations)
+station_t_df.to_csv(path.join(tests_data_dir, "station-t.csv"))
